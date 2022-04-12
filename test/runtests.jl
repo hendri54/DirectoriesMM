ab324a509cf88d449cd314fd75547bed18ad2928
hendri54 <hendricksl@protonmail.com>
2022-04-12 10:19:58 -0400

    First registered version

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using DirectoriesMM
 using Test
 
-@testset "DirectoriesMM.jl" begin
-    # Write your tests here.
+function directories_test()
+    @testset "Directories" begin
+        @test isdir(base_dir())
+        @test endswith(base_dir(), "mismatch");
+
+        @test isdir(project_dir())
+        @test isdir(computer_log_dir())
+        @test base_dir(:current) == base_dir()
+        @test isdir(computer_out_dir())
+        remoteOutDir = computer_out_dir(:longleaf)
+        @test startswith(remoteOutDir, "/nas/longleaf")
+        @test isdir(computer_mat_dir())
+    end
 end
+
+@testset "Directories" begin
+    directories_test();
+end
+
+# ---------------
